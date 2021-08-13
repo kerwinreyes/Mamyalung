831331032bf0f098b559936ee8d740ea56cb38b2
kdreyes1 <69441120+kdreyes1@users.noreply.github.com>
2021-08-13 14:46:42 +0800

    Enter

---
+++ lib/materials.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+final ButtonStyle style = ElevatedButton.styleFrom(
+  textStyle: const TextStyle(fontSize: 20));
+final Color primaryBlue = const Color.fromRGBO(29, 58, 160, 1.0);
+final Color lightBlue = const Color.fromRGBO(100, 156, 222, 1.0);
+final Color green = const Color.fromRGBO(35, 134, 54, 1.0);
+final Color lightgreen = const Color.fromRGBO(113, 198, 56, 1.0);
+final Color orange = const Color.fromRGBO(237,175,75, 1.0);
+final Color red = const Color.fromRGBO(224,85,100, 1.0);
+final Color black = const Color.fromRGBO(17,19,27, 1.0);
+final Color gray = const Color.fromRGBO(26,29,40, 1.0);
+final Color white = const Color.fromRGBO(240,229,211, 1.0);
+
+
