cde2effafb5c66f64f8a391035d30f2dd6b62cd2
kdreyes1 <69441120+kdreyes1@users.noreply.github.com>
2021-08-17 00:58:16 +0800

    Save

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,41 @@ class DashBoardPage extends StatefulWidget {
 }
 
 class _DashBoardPageState extends State<DashBoardPage> {
+
   bool _isSigningOut=false;
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('users').snapshots();
+  final db = FirebaseFirestore.instance;
+  List userProfileList=[];
+  @override
+  void initState(){
+    fetchDatabaseList();
+    super.initState();
+  }
+  ggetUser() async {
+    List a = [];
+    await FirebaseFirestore.instance
+      .collection('users')
+      .get()
+      .then((QuerySnapshot querySnapshot){
+         querySnapshot.docs.forEach((DocumentSnapshot doc) {
+           a.add(doc.data());
+           print(a[0]['fname']);
+           print(doc.exists);
+           print(doc.id);
+        }); 
+      });
+  }
+  fetchDatabaseList() async {
+    dynamic resultant = await UserManagement().getUsersList();
+    if(resultant == null){
+      print('unable to retrieve');
+    }else{
+        userProfileList = resultant;
+        print(userProfileList[0]);
+             
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,8 +115,50 @@ class _DashBoardPageState extends State<DashBoardPage> {
         )
       ),
       body: Center(
-        child:Text('DashBoard Page')
+        child: ListView(
+          children: [
+            Text('Manage Users'),
+            UserInformation(),
+            Text('Pakyu')
+          ],
+        ),
       ),
     );
+  }
+}
+
+class UserInformation extends StatefulWidget {
+  @override
+    _UserInformationState createState() => _UserInformationState();
+}
+
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('users').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+        final data =snapshot.requireData;
+        return ListView.builder(
+          itemCount: data.size,
+          itemBuilder: (context, index){
+            print(data.docs[index]['fname']);
+            return Text(data.docs[index]['fname']);
+          },
+        );
+      },
+    ));
   }
 }
--- lib/services/usermanagement.dart
+++ lib/services/usermanagement.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -47,5 +48,34 @@ class UserManagement{
   }
   signOut(){
     FirebaseAuth.instance.signOut();
+  }
+  Future getUsersList() async{
+    List itemslist =[];
+    var user = FirebaseAuth.instance.currentUser;
+    try{
+      if(user != null){
+      await FirebaseFirestore.instance
+      .collection('users')
+      .get()
+      .then((QuerySnapshot querySnapshot){
+         querySnapshot.docs.forEach((DocumentSnapshot doc) {
+           itemslist.add(doc.data());
+        }); 
+      });
+      return itemslist;
+    }
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+}
+class DatabaseProfile{
+  final String uid;
+  DatabaseProfile({required this.uid});
+
+  final CollectionReference profile = FirebaseFirestore.instance.collection("users");
+  Future updateUserData(String fname, String lname, String password) async {
+    return ;
   }
 }
